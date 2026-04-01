7f6f7ac0c8acbb5cb93b5f2c8e5824c3551a726a
Noé Fischer <noe.fischer@gmail.com>
2026-04-01 21:51:30 +0200

    fix formatting and naming

--- models/bronze/stg_orders.sql
+++ models/bronze/stg_orders.sql
@@ -4,12 +4,12 @@ with source as (
 
 renamed as (
     select
-        cast(order_id    as bigint)         as order_id,
-        cast(customer_id as bigint)         as customer_id,
-        cast(product_id  as bigint)         as product_id,
-        cast(amount      as decimal(10, 2)) as amount,
-        cast(status      as varchar)        as status,
-        cast(created_at  as date)           as created_at
+        cast(order_id as bigint) as order_id,
+        cast(customer_id as bigint) as customer_id,
+        cast(product_id as bigint) as product_id,
+        cast(amount as decimal(10, 2)) as amount,
+        cast(status as varchar) as status,
+        cast(created_at as date) as created_at
     from source
 )
 
